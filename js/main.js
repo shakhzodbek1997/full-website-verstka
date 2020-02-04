45efff9428399308e4d85dffefb12e3aa9352f50
Shakhzodbek Yuldoshev <shakhzodbek.yuldoshev@gmail.com>
2020-02-04 09:59:22 +0500

    section news

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(function () {
 
-         $('.slider_inner').slick({
+         $('.slider_inner, .news_slider-inner').slick({
              nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
              prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
              infinite: false
          });
 
          $('select').styler();
+
+
+
 });
